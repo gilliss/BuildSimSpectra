f93b1fb792737ccc56aaec1bfcb93030bc323bd9
Tom Gilliss <gilliss@live.unc.edu>
2018-01-31 15:35:11 -0500

    minor

--- Macro_MJD_decayChainCombined.py
+++ Macro_MJD_decayChainCombined.py
@@ -1,30 +1,11 @@
 """
 Mimic hierarchy of ordering used for creation of MJD_<decayChain>Combined.* hists
-As shown in the two code blocks below, can achieve equivalent results with different syntax regarding decayChain and segment looping
 """
 
 import BaseClasses.BSLoop as BSLoop
 bslp = BSLoop.BSLoop()
 
-# # For each detector, loop each hardwareComponent; and within each hardwareComponent, loop each decayChain's segments
-# # lowest level of recursion
-# recurSegment = {'r_objType': 'segment', 'r_weightFunc': None, 'r_recur': {}} # end recursion upon {}
-# # second lowest level of recursion
-# recurHardwareComponent = {'r_objType': 'hardwareComponent', 'r_weightFunc': None, 'r_recur': recurSegment}
-# # top level of recursion
-# bslp.For(objType = 'detector', weightFunc = None, **recurHardwareComponent)
-#
-# # EQUIVALENTLY: For each detector, loop each hardwareComponent; and within each hardwareComponent, loop each decayChain's segments
-# # lowest level of recursion
-# recurSegment = {'r_objType': 'segment', 'r_weightFunc': None, 'r_recur': {}} # end recursion upon {}
-# # second lowest level of recursion
-# recurDecayChain = {'r_objType': 'decayChain', 'r_weightFunc': None, 'r_recur': recurSegment}
-# # third lowest level of recursion
-# recurHardwareComponent = {'r_objType': 'hardwareComponent', 'r_weightFunc': None, 'r_recur': recurDecayChain}
-# # top level of recursion
-# bslp.For(objType = 'detector', weightFunc = None, **recurHardwareComponent)
-
-# BREAKING IT UP INTO STEPS SO CAN SAVE INTERMEDIATE PLOTS
+# BREAK THE PROCESS UP INTO STEPS SO CAN SAVE INTERMEDIATE PLOTS
 print('===================')
 print('...Set the configuration and cut')
 cV = {'configuration': 'DS1', 'cut': 1}
---
+++ Macro_example_decayChain_segment.py
@@ -0,0 +1,24 @@
+"""
+As shown in the two code blocks below, can achieve equivalent results with different syntax regarding decayChain and segment looping
+"""
+
+import BaseClasses.BSLoop as BSLoop
+bslp = BSLoop.BSLoop()
+
+# For each detector, loop each hardwareComponent; and within each hardwareComponent, loop each decayChain's segments
+# lowest level of recursion
+recurSegment = {'r_objType': 'segment', 'r_weightFunc': None, 'r_recur': {}} # end recursion upon {}
+# second lowest level of recursion
+recurHardwareComponent = {'r_objType': 'hardwareComponent', 'r_weightFunc': None, 'r_recur': recurSegment}
+# top level of recursion
+bslp.For(objType = 'detector', weightFunc = None, **recurHardwareComponent)
+
+# EQUIVALENTLY: For each detector, loop each hardwareComponent; and within each hardwareComponent, loop each decayChain's segments
+# lowest level of recursion
+recurSegment = {'r_objType': 'segment', 'r_weightFunc': None, 'r_recur': {}} # end recursion upon {}
+# second lowest level of recursion
+recurDecayChain = {'r_objType': 'decayChain', 'r_weightFunc': None, 'r_recur': recurSegment}
+# third lowest level of recursion
+recurHardwareComponent = {'r_objType': 'hardwareComponent', 'r_weightFunc': None, 'r_recur': recurDecayChain}
+# top level of recursion
+bslp.For(objType = 'detector', weightFunc = None, **recurHardwareComponent)
