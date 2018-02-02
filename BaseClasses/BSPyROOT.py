c6b89be8fccb2a8f6e2660382c1fa904d7372628
Tom Gilliss <gilliss@live.unc.edu>
2018-02-02 09:43:22 -0500

    minor

--- BaseClasses/BSPyROOT.py
+++ BaseClasses/BSPyROOT.py
@@ -8,3 +8,11 @@ This class should be able to convert root into pandas or numpy objects and retur
 class BSPyROOT():
     def __init__(self):
         return None
+
+
+
+# NOTES
+   # KEY: TCanvas  c1;1    A canvas
+   # OBJ: TH1D    h1      Bellows:A210_Z81:1010102 : 1 at: 0x26b1cf0
+   # OBJ: TH1D    h2      Granularity : 1 at: 0x2758620
+   # OBJ: TH1D    h3      Gran+PSA : 1 at: 0x276c3d0
