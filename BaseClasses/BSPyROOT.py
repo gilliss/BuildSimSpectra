70bd37e2be413a61388c767370a40bf53a0ec547
Tom Gilliss <gilliss@live.unc.edu>
2018-02-02 12:23:06 -0500

    minor

--- BaseClasses/BSPyROOT.py
+++ BaseClasses/BSPyROOT.py
@@ -44,7 +44,7 @@ class BSPyROOT():
         xArray = np.arange(self.xmin + 0.5, self.xmax + 0.5) # to be used as list of bin edges (np treats last number as INCLUDED upper edge of last been)
 
         plt.step(xArray, hArray, where = 'mid', color='k')
-        plt.set_yscale('log', nonposy='clip')
+        plt.yscale('log')#, nonposy='clip')
         #ax.set_xlim(0, 3000)
 
         figName = '%s_%s_%s_%s.pdf' % (hardwareComponent, segment, detector, str(cut))
