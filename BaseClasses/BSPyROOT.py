07190cf13cb05cc46fa077d89d58d58173f97128
Tom Gilliss <gilliss@live.unc.edu>
2018-04-12 16:20:17 -0400

    trimmed off under and overflow bins in BSPyROOT

--- BaseClasses/BSPyROOT.py
+++ BaseClasses/BSPyROOT.py
@@ -20,8 +20,8 @@ class BSPyROOT():
         self.hardwareGroup = None
 
         self.nBinsX = 10000
-        self.xmin = 0.0 # low edge, included
-        self.xmax = 10000.0 # upper edge, excluded
+        self.xmin = 0.0 # low edge (included)
+        self.xmax = 10000.0 # upper edge (excluded in ROOT, included in np and plt)
 
         return None
 
@@ -47,11 +47,14 @@ class BSPyROOT():
         c = f.Get('c1')
         hName = 'h' + str(cut)
         h = c.GetPrimitive(hName)
+        if (h.GetNbinsX() != self.nBinsX) or (h.GetNcells() != self.nBinsX + 2):
+            self.Print(0, 'Error', '  GetBinnedData: Unexpected binning', inFile, hName)
 
         hArray = np.zeros(self.nBinsX)
         if h.GetEntries() > 0:
             self.Print(3, 'Debug', '  Working with:', h.GetName(), h.GetTitle(), h.GetNbinsX(), h.GetEntries(), h.Integral())
-            hArray = np.frombuffer(h.GetArray(), dtype = 'float', count = self.nBinsX, offset = 0) # getting array of data from PyDoubleBuffer object
+            hArray = np.frombuffer(h.GetArray(), dtype = 'float', count = self.nBinsX + 2, offset = 0) # getting array of data from PyDoubleBuffer object
+            hArray = hArray[1:-1] # trimming off underflow and overflow bins 
         else:
             self.Print(3, 'Debug','  0 entries. Perhaps 0 MC hits? Returning hArray = zeros.')
 
@@ -64,16 +67,20 @@ class BSPyROOT():
         return hArray
 
 # NOTES
+    # numpy and matplotlib.pyplot :
+    # All but the last (righthand-most) bin is half-open. The first bin is [1, 2) -- [including, excluding)-- and the second [2, 3). The last bin, however, is [3, 4], which includes 4.
     # where : [ ‘pre’ | ‘post’ | ‘mid’ ]
     # If ‘pre’ (the default), the interval from x[i] to x[i+1] has level y[i+1].
     # If ‘post’, that interval has level y[i].
     # If ‘mid’, the jumps in y occur half-way between the x-values.
 
+    # ROOT :
     # For all histogram types: nbins, xlow, xup:
     # bin = 0;       underflow bin
     # bin = 1;       first bin with low-edge xlow INCLUDED
     # bin = nbins;   last bin with upper-edge xup EXCLUDED
     # bin = nbins+1; overflow bin
+    # fNcells; ///< number of bins(1D), cells (2D) +U/Overflows
     #
     # root [27] h->GetXaxis()->GetBinLowEdge(1)
     # (double) 0.0000000
@@ -84,8 +91,9 @@ class BSPyROOT():
     # root [30] h->GetXaxis()->GetBinLowEdge(10000)
     # (double) 9999.0000
 
-   # FILES LIKE: DUCopper_A210_Z81_1010102.root:
-   # KEY: TCanvas  c1;1    A canvas
-   # OBJ: TH1D    h1      Bellows:A210_Z81:1010102 : 1 at: 0x26b1cf0
-   # OBJ: TH1D    h2      Granularity : 1 at: 0x2758620
-   # OBJ: TH1D    h3      Gran+PSA : 1 at: 0x276c3d0
+    # MJD simulations output :
+    # FILES LIKE: DUCopper_A210_Z81_1010102.root:
+    # KEY: TCanvas  c1;1    A canvas
+    # OBJ: TH1D    h1      Bellows:A210_Z81:1010102 : 1 at: 0x26b1cf0
+    # OBJ: TH1D    h2      Granularity : 1 at: 0x2758620
+    # OBJ: TH1D    h3      Gran+PSA : 1 at: 0x276c3d0
